f47ae86d8ef4fc6a0cd1ceebe351691d67b4506f
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2023-08-20 21:02:21 +0800

    php

---
+++ Practice/09.function.php
@@ -0,0 +1,13 @@
+
+<?php   
+
+function inforMation($name,$age)
+{
+    echo "Hi my name is {$name} i am {$age} Years of Age <br>" ; 
+}
+
+inforMation("Daniel",19);
+inforMation("Justine",21);
+inforMation("Lloyd",21);
+
+?>
---
+++ Practice/10.function.With.Return.php
@@ -0,0 +1,13 @@
+<?php
+    //PARAMETE IS $VALUE1,$VALUE2
+    function answer($value1,$value2)
+    {
+        $result = $value1 + $value2;
+        //This will return the Answer
+        return $result;
+    }
+    //This is Argument that will pas into the Function Parameter
+   echo answer(1,3);
+
+ 
+?>
